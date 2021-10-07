1c6d596bbeb6d91d9e97c01de238122106f492b9
Leonardo <leocv0205@gmail.com>
2021-10-07 17:08:58 -0500

    Se crea el repositorio Stepverifier

---
+++ StepVerifier/src/main/java/com/sofka/StepVerifier/StepVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.StepVerifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepVerifierApplication.class, args);
+	}
+
+}
---
+++ StepVerifier/src/test/java/com/sofka/StepVerifier/StepVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.StepVerifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
